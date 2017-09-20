625fef5775107811ae8591a13821a08948b8168f
Jevri <31509799+Jevri@users.noreply.github.com>
2017-09-20 10:32:47 +0200

    fix

--- client/scripts.js
+++ client/scripts.js
@@ -141,7 +141,9 @@ $( ".modal-footer button" ).on( "click", function(){
   var color = $("#color-picker").val();
   backgroundColor = $("#color-picker-background").val();
   console.log(backgroundColor);
-  stroke(backgroundColor - "#FFFFFF");
+  var strokeColor = color(backgroundColor);
+  var nc = color(255-red(strokeColor),255-green(strokeColor) ,255-blue(strokeColor) );
+  stroke(nc));
   newPlayer(name, color);
   $("#new-user-modal").modal("hide")
 } );
